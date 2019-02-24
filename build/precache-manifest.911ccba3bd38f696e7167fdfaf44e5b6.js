b452a43469770998959d789e3dea469b72cec0b0
MAKSYM_enko <finside41@gmail.com>
2019-02-24 20:20:52 +0200

    gh-pages deploy was automared

---
+++ build/precache-manifest.911ccba3bd38f696e7167fdfaf44e5b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2e4160c1d6ee9dec8bb8",
+    "url": "/tictactoe-react/static/js/runtime~main.2e4160c1.js"
+  },
+  {
+    "revision": "c4e5fb3c7489a82a0919",
+    "url": "/tictactoe-react/static/js/main.c4e5fb3c.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/tictactoe-react/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "c4e5fb3c7489a82a0919",
+    "url": "/tictactoe-react/static/css/main.38289019.chunk.css"
+  },
+  {
+    "revision": "13645943c76feaba797c286b3dbd78ea",
+    "url": "/tictactoe-react/index.html"
+  }
+];
