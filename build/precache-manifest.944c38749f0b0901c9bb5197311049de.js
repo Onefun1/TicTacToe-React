a95af38e2adf29cadcf1135e133abe4c851baa3f
MAKSYM_enko <finside41@gmail.com>
2019-02-24 20:51:54 +0200

    some changes

---
+++ build/precache-manifest.944c38749f0b0901c9bb5197311049de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e4160c1d6ee9dec8bb8",
+    "url": "/tictactoe-react/static/js/runtime~main.2e4160c1.js"
+  },
+  {
+    "revision": "321ca7d6c88bd58687f8",
+    "url": "/tictactoe-react/static/js/main.321ca7d6.chunk.js"
+  },
+  {
+    "revision": "227790b2be4741aac0c1",
+    "url": "/tictactoe-react/static/js/2.227790b2.chunk.js"
+  },
+  {
+    "revision": "321ca7d6c88bd58687f8",
+    "url": "/tictactoe-react/static/css/main.21c10eb3.chunk.css"
+  },
+  {
+    "revision": "cbe6c647d00e2161588bafc8c95172f0",
+    "url": "/tictactoe-react/index.html"
+  }
+];
